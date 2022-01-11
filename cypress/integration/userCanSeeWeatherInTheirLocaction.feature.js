0903fab8da0f9b348d10fac8b04be39e2742b114
miyeaier <miyesier@gmail.com>
2022-01-11 12:36:24 +0100

    add semanic ui and test and all test green

--- cypress/integration/userCanSeeWeatherInTheirLocaction.feature.js
+++ cypress/integration/userCanSeeWeatherInTheirLocaction.feature.js
@@ -1,16 +1,18 @@
-
- describe("Weather info for usecp\'s location",()=>{
-   beforeEach(()=>{
-     cy.intercept('https://api.openweathermap.org/data/2.5/**',{ fixture: "weather_response.json"})
-     cy.intercept("https://api.opencagedata.com/geocode/v1/json/**",{ fixture: "location_response.json"})
-   })
-  it("is expected to be displayed on the intial render", () => {
+describe("Weather info for usecp\'s location", () => {
+  beforeEach(() => {
+    cy.intercept('https://api.openweathermap.org/data/2.5/**', {
+      fixture: "weather_response.json"
+    })
+    cy.intercept("https://api.opencagedata.com/geocode/v1/json/**", {
+      fixture: "location_response.json"
+    })
     cy.visit("http://localhost:3000", {
       onBeforeload(window) {
         const stubLocation = {
-          coords: { latitiude: 57.7308, longitute: 11.9834 },
-
-   
+          coords: {
+            latitiude: 57.7308,
+            longitute: 11.9834
+          },
         };
         cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
           (callback) => {
@@ -19,6 +21,15 @@
         );
       },
     });
+  })
+  it("is expected to be displayed on the intial render", () => {
+    cy.get("[data-cy=title]").should("contain", "Weather")
   });
+  it('is expected to display a table', () => {
+    cy.get("[data-cy=table]").should("be.visible")
+    cy.get("[data-cy=Country]").should("contain", "Sweden");
+    cy.get("[data-cy=City]").should("contain", "Stockholm");
+    cy.get("[data-cy=Temperature]").should("contain", "");
 });
-  
+
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Divider, Header, Icon, Table } from "semantic-ui-react";
 
 class App extends Component {
   state = {
@@ -7,9 +8,9 @@ class App extends Component {
   };
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(async position => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
       let { latitude, longitude } = position.coords;
-      
+
       let locationResponse = await axios.get(
         `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=fea8964d93c5492aa5eb1586b96eab48`
       );
@@ -24,13 +25,35 @@ class App extends Component {
         temp: weatherResponse.data.current.temp,
       };
       this.setState({ location: weatherInfo });
-      debugger
     });
   }
   render() {
     return (
-      <div >
-    </div>
+      <>
+        <Divider horizontal>
+          <Header as="h4" data-cy="title">
+            <Icon name="wpexplorer" />
+            Weather
+          </Header>
+        </Divider>
+
+        <Table definition data-cy="table">
+          <Table.Body>
+            <Table.Row data-cy="Country">
+              <Table.Cell width={2} >Country</Table.Cell>
+              <Table.Cell>{this.state.location?.country}</Table.Cell>
+            </Table.Row>
+            <Table.Row data-cy="City">
+              <Table.Cell >City</Table.Cell>
+              <Table.Cell>{this.state.location?.city}</Table.Cell>
+            </Table.Row>
+            <Table.Row data-cy="Temperature">
+              <Table.Cell>Temperature</Table.Cell>
+              <Table.Cell>{this.state.location?.temp}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </>
     );
   }
 }
